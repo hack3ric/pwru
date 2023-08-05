646ef74300333c274b7829c0ce58b73e13116d1d
Eric Long <i@hack3r.moe>
2023-08-06 01:54:12 +0800

    Add vmlinux-riscv64.h
    Generated on Linux 6.4.

--- bpf/headers/vmlinux.h
+++ bpf/headers/vmlinux.h
@@ -2,6 +2,8 @@
 #include "vmlinux-x86.h"
 #elif defined(__TARGET_ARCH_arm64)
 #include "vmlinux-arm64.h"
+#elif defined(__TARGET_ARCH_riscv)
+#include "vmlinux-riscv.h"
 #else
 #error "Unknown architecture"
 #endif
